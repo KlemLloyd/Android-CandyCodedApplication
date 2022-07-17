c41147af9cc490b4bfab54cebd94fcbfe9df9ddd
KlemLloyd <Klem.elim@gmail.com>
2022-07-17 08:34:23 +0200

    Share the Current Candy with an Intent

--- app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
@@ -7,11 +7,13 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import com.pluralsight.candycoded.DB.CandyContract;
 import com.pluralsight.candycoded.DB.CandyContract.CandyEntry;
 import com.pluralsight.candycoded.DB.CandyDbHelper;
@@ -79,7 +81,10 @@ public class DetailActivity extends AppCompatActivity {
     private void createShareIntent() {
         Intent shareIntent = new Intent(Intent.ACTION_SEND);
         shareIntent.setType("text/plain");
-        shareIntent.putExtra(Intent.EXTRA_TEXT, SHARE_DESCRIPTION + mCandyImageUrl + HASHTAG_CANDYCODED);
+        shareIntent.putExtra(
+                Intent.EXTRA_TEXT,
+                SHARE_DESCRIPTION + mCandyImageUrl + HASHTAG_CANDYCODED
+        );
         startActivity(shareIntent);
     }
 }
