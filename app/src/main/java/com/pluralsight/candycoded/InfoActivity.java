cbdb49563359d76aa2e2ae9b522922d313e72d87
KlemLloyd <Klem.elim@gmail.com>
2022-07-17 07:33:16 +0200

    Launch the Google Maps Activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -34,9 +34,8 @@ public class InfoActivity extends AppCompatActivity {
     public void createMapIntent(View view) {
        Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, uri);
-        Intent result = mapIntent.setPackage("com.google.android.apps.maps");
-        if (result != null) {
-            mapIntent.resolveActivity(getPackageManager());
+        mapIntent.setPackage("com.google.android.apps.maps");
+        if (mapIntent.resolveActivity(getPackageManager()) != null) {
             startActivity(mapIntent);
         }
     }
